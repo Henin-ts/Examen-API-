255da6fc4a7549b7c4d017ba1fc4a8713fdd7f7f
henintsoa <Henintsoa@example.com>
2025-07-31 09:09:22 +0300

    commit 2

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+
+@app.get("/ping")
+async def ping():
+    return PlainTextResponse("pong", status_code=200)
+
+
+@app.get("/home", response_class=HTMLResponse)
+async def home():
+    return HTMLResponse(content="<h1>Welcome home!</h1>", status_code=200)
+
+
