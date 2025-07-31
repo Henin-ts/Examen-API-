13da23a0ad15fa24da340d9c0ec2cc3222d7c3f6
henintsoa <Henintsoa@example.com>
2025-07-31 09:35:32 +0300

    modification commit 3

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ async def ping():
 
 @app.get("/home", response_class=HTMLResponse)
 async def home():
-    return HTMLResponse(content="<h1>Welcome home!</h1>", status_code=200)
+    return HTMLResponse(content=" <h1> Welcome home! </h1> ", status_code=200)
 
 @app.exception_handler(404)
 async def custom_404_handler(request):
-    return HTMLResponse(content="<h1>404 NOT FOUND</h1>",status_code=404)
+    return HTMLResponse(content=" <h1> 404 NOT FOUND </h1> ",status_code=404)
 
 @app.post("/posts")
 async def posts( author : str , title : str , content : str ):
