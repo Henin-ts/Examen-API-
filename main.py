9fd144d86da60ec0342854ed28f28bb8c1819569
henintsoa <Henintsoa@example.com>
2025-07-31 09:33:13 +0300

    commit 3

--- main.py
+++ main.py
@@ -13,4 +13,10 @@ async def ping():
 async def home():
     return HTMLResponse(content="<h1>Welcome home!</h1>", status_code=200)
 
+@app.exception_handler(404)
+async def custom_404_handler(request):
+    return HTMLResponse(content="<h1>404 NOT FOUND</h1>",status_code=404)
 
+@app.post("/posts")
+async def posts( author : str , title : str , content : str ):
+    return 
